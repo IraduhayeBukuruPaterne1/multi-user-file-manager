1c4e488ca5cb72c86565b0fd25d067db8be10df4
Iraduhaye Bukuru Paterne <116680462+IraduhayeBukuruPaterne1@users.noreply.github.com>
2024-11-27 21:42:06 +0000

    paterne

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+app.post('/register', (req, res) => {
+    // Simulate user registration
+    res.send('User registered!');
+});
+
+app.post('/login', (req, res) => {
+    // Simulate user login
+    res.send('User logged in!');
+});
